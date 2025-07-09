709b951e625752450cfc21b346ab44f59a0bc595
Kamran Nazir <kamrannazir901@gmail.com>
2025-07-09 21:17:20 +0500

    Deploy portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages"; // ✅ import this
+// import { ghPages } from "vite-plugin-gh-pages"; // ✅ import this
 
 export default defineConfig({
   base: "/personal-portfolio-ui/",
 
-  plugins: [react(), ghPages()],
+  plugins: [react()],
 });
